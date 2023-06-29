52175f51e6c682598157f76c488a1606de387985
Sayali <saykaw16@gmail.com>
2023-06-29 21:12:59 +0530

    pattern 4

---
+++ pattern_based/pattern4.cpp
@@ -0,0 +1,25 @@
+// 9 8 7 
+// 6 5 4
+// 3 2 1
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int i=1;
+    int count = n*n;
+    while(i<=n)
+    {
+        int j = 1;
+        while(j<=n)
+        {
+            cout << count << " ";
+            count = count-1;
+            j = j+1;
+        }
+        cout << endl;
+        i = i+1;
+    }
+}
