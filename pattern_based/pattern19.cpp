4e8cccf47dbc5c1dd6ea8e086fed1f1188c6b24f
Sayali <saykaw16@gmail.com>
2023-07-01 12:53:50 +0530

    pattern 19

---
+++ pattern_based/pattern19.cpp
@@ -0,0 +1,25 @@
+// * * * *
+// * * *
+// * *
+// *
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int i=1;
+    while(i<=n)
+    {
+        int j=n-i+1;
+        while(j>0)
+        {
+            cout << "* ";
+            j=j-1;
+        }
+        cout<<endl;
+        i=i+1;
+    }
+    return 0;
+}
