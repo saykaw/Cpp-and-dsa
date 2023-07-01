e49cb1f3d20dd48b17d37c9bc207366516ce33c0
Sayali <saykaw16@gmail.com>
2023-07-01 12:54:27 +0530

    pattern 21

---
+++ pattern_based/pattern21.cpp
@@ -0,0 +1,31 @@
+// 1 1 1 1
+//   2 2 2
+//     3 3
+//       4
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int i = 1;
+    while(i<=n)
+    {
+        int space = 1;
+        while(space<i)
+        {
+            cout << " ";
+            space = space +1;
+        }
+        int j=n-i+1;
+        while(j>0)
+        {
+            cout << i;
+            j=j-1;
+        }
+        cout << endl;
+        i=i+1;
+    }
+    return 0;
+}
